bac3e3279dd14dce4b0a0218f3482ce6d8db2c09
Justin Day <justin@jplt.com>
2013-03-03 17:01:39 -0500

    Refactored to use divs.

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 var APP = (function() {
 	var self = {};
-	var labelIdx = 0;
-	var textIdx = 0;
+	var canvas;
+	var ctx;
 
-	function setHexClip(hex) {
-		var ctx = hex.getContext('2d');
+	function getBackgroundImageSrc(element) {
+		var url = $(element).css('background-image');
+		
+		if (/([\w\-\.]+\.\w{3})['"]?\)?$/.test(url)) 
+			return RegExp.$1;	
+		else
+			return null;
+	}
 
+	function getBackgroundPosition(element) {
+		var pos = $(element).css('background-position');
+		var parts = pos.split(/ /);
+
+		return { 
+			x: parts[0].replace('px','').replace('%',''),
+			y: parts[1].replace('px','').replace('%','')
+		};
+	}
+
+	function setHexClip() {
 		ctx.beginPath();
    		ctx.moveTo(40,0);
    		ctx.lineTo(120,0);
@@ -18,40 +35,41 @@ var APP = (function() {
 	}
 
 	function paintHexBackground(hex) {
-		var img = $('img[src="' + hex.getAttribute('data-img') + '"]').get(0);
+		var src = getBackgroundImageSrc(hex);
+		
+		if (src) {
+			var color = $(hex).css('background-color');
+			$(hex).css('background-color','inherit');
+			var img = $('img[src="' + src + '"]').get(0);
+			var pos = getBackgroundPosition(hex);
 
-		var ctx = hex.getContext('2d');
-		var x = hex.getAttribute('data-offset-x') || 0;
-		var y = hex.getAttribute('data-offset-y') || 0;
-		ctx.drawImage(img, x, y);
-	}
+			ctx.clearRect(0,0,160,140);
+			ctx.drawImage(img, pos.x, pos.y);
 
-	function paintHexColor(hex) {
-		var ctx = hex.getContext('2d');
-		ctx.fillStyle = $(hex).css('color');
-		ctx.fillRect(0,0,160,140);
-	}
+			var data = "url(" + canvas.toDataURL('image/png') + ")";
+			$(hex).css('background-image', data).css('background-position','0px 0px');
 
-	function paintHexLabelBackground(hex) {
-		var ctx = hex.getContext('2d');
-		ctx.save();
-		ctx.fillStyle = 'rgba(255,255,255,0.75)';
-		ctx.fillRect(0,120,160,20);
-		ctx.restore();
-	}
+			ctx.clearRect(0,0,160,140);
+			ctx.fillStyle = color;
+			ctx.fillRect(0,0,160,140);
 
-	function paintHexTextBackground(hex) {
-		var ctx = hex.getContext('2d');
-		ctx.save();
-		ctx.fillStyle = 'rgba(255,255,255,0.75)';
-		ctx.fillRect(0,55,160,30);
-		ctx.restore();
+			$(hex).append($('<img src="' + canvas.toDataURL('image/png') + '" />'));
+			$(hex).mouseover(function() { onHexOver(hex); });
+			$(hex).mouseout(function() { onHexOut(hex); });
+			$(hex).click(function() { onHexClick(hex); });
+		}
+		else {
+			ctx.clearRect(0,0,160,140);
+			paintHexBorder(hex);
+
+			var data = "url(" + canvas.toDataURL('image/png') + ")";
+			$(hex).css('background-image', data).css('background-position','0px 0px');
+		}
 	}
 
 	function paintHexBorder(hex) {
-		var ctx = hex.getContext('2d');
 		ctx.save();
-		ctx.strokeStyle = $(hex).css('color');
+		ctx.strokeStyle = $(hex).find('.label').css('color');
 		ctx.lineWidth = 5;
 		ctx.beginPath();
    		ctx.moveTo(40,0);
@@ -65,76 +83,12 @@ var APP = (function() {
     	ctx.restore();
 	}
 
-	function appendHexLabelText(hex) {
-		var pos = $(hex).offset();
-		pos.top += $(hex).height() + parseInt(hex.getAttribute('data-label-offset-y') || -15);
-
-		var element = $('<div>' + $(hex).html() + '</div>');
-		element.addClass($(hex).attr('class'));
-		element.addClass('text');
-		element.css('position','absolute');
-		element.width(160);
-		element.height(20);
-		element.offset(pos);
-		element.css('z-index', 100);
-		element.attr('id', 'label-' + labelIdx++);
-		$(hex).attr('data-label-id', element.attr('id'));
-		element.mouseover(function() { onHexOver(hex); });
-		element.mouseout(function() { onHexOut(hex); });
-		element.click(function() { onHexClick(hex); });
-
-		$('body').append(element);
-	}
-
-	function appendHexText(hex) {
-		var pos = $(hex).offset();
-		pos.top += parseInt(hex.getAttribute('data-text-offset-y') || 60);
-
-		var element = $('<div>' + $(hex).html() + '</div>');
-		element.addClass($(hex).attr('class'));
-		element.addClass('text');
-		element.css('position','absolute');
-		element.width(160);
-		element.height(40);
-		element.offset(pos);
-		element.css('z-index', 100);
-		element.attr('id', 'text-' + textIdx++);
-		$(hex).attr('data-text-id', element.attr('id'));
-		element.mouseover(function() { onHexOver(hex); });
-		element.mouseout(function() { onHexOut(hex); });
-		element.click(function() { onHexClick(hex); });
-		
-		$('body').append(element);
-
-		return element;
-	}
-
-	function getText(hex) {
-		return $('#'+$(hex).attr('data-text-id'));
-	}
-
-	function getLabel(hex) {
-		return $('#'+$(hex).attr('data-label-id'));
-	}
-
 	function onHexOver(hex) {
 		$(hex).addClass('over');
-
-		paintHexBackground(hex);
-
-		paintHexColor(hex);
-		paintHexTextBackground(hex);
-		getText(hex).show();
-		getLabel(hex).hide();
 	}
 
 	function onHexOut(hex) {
 		$(hex).removeClass('over');
-		paintHexBackground(hex);
-		paintHexLabelBackground(hex);
-
-		getLabel(hex).show();
-		getText(hex).hide();
 	}
 
 	function onHexClick(hex) {
@@ -147,35 +101,17 @@ var APP = (function() {
 
 	function buildHexes() {
 		$('.hex').each(function(idx, hex) { 
-			hex.width = 160;
-			hex.height = 140;
-
-			setHexClip(hex);
-
-			if (hex.getAttribute('data-img')) {
-				paintHexBackground(hex);
-				paintHexLabelBackground(hex); 
-				appendHexLabelText(hex);
-
-				$(hex).addClass('over');
-				var text = appendHexText(hex).hide();
-				$(hex).removeClass('over');
-				text.css('color','black');
-
-				hex.onmouseover = function() { onHexOver(hex); }
-				hex.onmouseout = function() { onHexOut(hex); }
-				hex.onclick = function() { onHexClick(hex); }
-			}
-			else {
-				paintHexBorder(hex);
-				appendHexText(hex);
-			}
+			paintHexBackground(hex);
 		});
 	}
 
-
 	function init() {
+		canvas = $('canvas')[0];
+		ctx = canvas.getContext('2d');
+
+		setHexClip();
 		$(function() { Kerning.live(); });
+
 		buildHexes();
 	}
 
